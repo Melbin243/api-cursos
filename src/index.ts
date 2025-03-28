ac8d4d39739c70413de12afddeb1afa085036b61
melbin243 <142106036+Melbin243@users.noreply.github.com>
2025-03-27 20:32:03 -0400

    Configure server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hola Mundo');
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan';
+
+const app = express();
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Servidor activo');
+})
