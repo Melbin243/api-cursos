da13d0496353b0d0862f242b69dd0d1c2e67b617
melbin243 <142106036+Melbin243@users.noreply.github.com>
2026-01-17 00:10:22 -0400

    crear mapper para la entidad estudiante

---
+++ src/main/java/com/api_cursos/mapper/EstudianteMapper.java
@@ -0,0 +1,48 @@
+package com.api_cursos.mapper;
+
+import com.api_cursos.dto.estudiante.EstudianteRequest;
+import com.api_cursos.dto.estudiante.EstudianteResponse;
+import com.api_cursos.persistence.entity.Curso;
+import com.api_cursos.persistence.entity.Estudiante;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+@Component
+public class EstudianteMapper {
+
+    public EstudianteResponse toResponse(Estudiante estudiante) {
+        if (estudiante == null) return null;
+
+        return new EstudianteResponse(
+                estudiante.getId(),
+                estudiante.getNombre(),
+                estudiante.getApellido(),
+                estudiante.getEmail(),
+                estudiante.getCursos().stream()
+                        .map(Curso::getNombre)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    public Estudiante toEntity(EstudianteRequest request) {
+        if (request == null) return null;
+
+        return Estudiante.builder()
+                .nombre(request.nombre().trim())
+                .apellido(request.apellido().trim())
+                .email(request.email().trim())
+                .cursos(new ArrayList<>())
+                .build();
+    }
+
+    public void updateEntity(EstudianteRequest request, Estudiante estudiante) {
+        if (request == null || estudiante == null ) return;
+
+        estudiante.setNombre(request.nombre().trim());
+        estudiante.setApellido(request.apellido().trim());
+        estudiante.setEmail(request.email().trim());
+    }
+
+}
